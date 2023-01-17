3089947ccef83fce5444f6b618d71c8596654ec8
Mustafa Babadagi <fathermountain@gmail.com>
2023-01-16 22:50:34 -0500

    new version

---
+++ src/main/java/stringmanipulations/StringBuilderAppend.java
@@ -0,0 +1,51 @@
+package stringmanipulations;
+
+public class StringBuilderAppend {
+
+    public static void main(String[] args) {
+
+
+        //Example 1:
+
+        StringBuilder strBld = new StringBuilder( "6");
+
+        strBld.append("Java");
+        strBld.append("Learn");
+
+        System.out.println(strBld);
+
+
+        //	Example 2:
+        StringBuilder strBld1 = new StringBuilder("John ");
+        strBld1.append("Woo ").append("Leo");
+        System.out.println(strBld1);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

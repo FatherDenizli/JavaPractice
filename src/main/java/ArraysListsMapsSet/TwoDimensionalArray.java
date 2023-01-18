91ebb6876c4d084c9bb8833b3c3567fc91c3e23a
Mustafa Babadagi <fathermountain@gmail.com>
2023-01-18 00:44:00 -0500

    new version

---
+++ src/main/java/ArraysListsMapsSet/TwoDimensionalArray.java
@@ -0,0 +1,35 @@
+package ArraysListsMapsSet;
+
+import java.util.Arrays;
+
+public class TwoDimensionalArray {
+
+    public static void main(String[] args) {
+
+        String brr[][]= { {"Sally", "Katty"} ,   {"Angie"} ,   {"Mary", "Chris", "Mark"} };
+
+add git .
+        System.out.println(Arrays.deepToString(brr));
+        System.out.println(brr[2][1]);
+        System.out.println(brr[0][0]);
+
+        System.out.println(Arrays.toString(brr[2]));
+
+
+        String crr[][]= { {"Sally", "Katty"} ,   {"Angie"} ,   {"Mary", "Chris", "Mark"} };
+
+        System.out.println(crr.length);
+        System.out.println(crr[1].length);
+        int sum=0;
+        for(String[] w:crr) {
+            sum=sum+w.length;
+
+        }
+        System.out.println(sum);
+
+
+    }
+
+
+
+}
